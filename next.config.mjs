9e1aba1a20581a1aad6bba90fe0f74367faf372c
jakewright01 <jakewright2306@icloud.com>
2025-07-05 00:21:53 +0100

    Fix CORS with New Frontend URL

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,7 @@ const nextConfig = {
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; connect-src 'self' http://localhost:8000 https://lhkffzhivpjbsufenncu.supabase.co https://sourcing-platform-api-jake.onrender.com https://api.ebay.com https://www.depop.com https://www.vinted.co.uk wss: ws: https:; img-src 'self' data: https:; font-src 'self';",
+            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; connect-src 'self' http://localhost:8000 https://lhkffzhivpjbsufenncu.supabase.co https://sourcing-platform-api-jake.onrender.com https://sourcing-platform-frontend-qgwxgmi9o-jakewright01s-projects.vercel.app https://api.ebay.com https://www.depop.com https://www.vinted.co.uk wss: ws: https:; img-src 'self' data: https:; font-src 'self';",
           },
           {
             key: 'X-Frame-Options',
